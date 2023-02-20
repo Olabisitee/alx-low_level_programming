a4d9a09f192ef190c4f1fdb28c0bd18ab68c0d8e
Olabisitee <oladepoolabisi2001@gmail.com>
2023-02-20 13:05:48 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - C Program that prints a new line with put function
+* Return: 0 (WellDone)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
