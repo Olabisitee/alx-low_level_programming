97ab80f52b00b9a46968aa752f9501b8cb265532
Olabisitee <oladepoolabisi2001@gmail.com>
2023-02-20 16:13:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
-  * main -A c program that prints
-  * Return: 1
-  */
+ * main - Prints "and music gives courage"
+ *
+ * Return: 1
+ */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
