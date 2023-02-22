0a08e0a34b16fc10414bf736a0371d2fb5330417
Olabisitee <oladepoolabisi2001@gmail.com>
2023-02-22 04:37:07 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: print alphabets in lowercase using the _putchar, 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int len = 26;
+	int counter;
+	int i = 0;
+
+	while (i < 10)
+	{
+		counter = 0;
+
+		while (counter < len)
+		{
+			_putchar(alphabets[counter]);
+			counter++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
