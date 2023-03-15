16ccd938c4c7863a0e1d1a79b21f50bd78c497cc
Olabisitee <oladepoolabisi2001@gmail.com>
2023-03-15 02:46:25 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,40 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * argstostr - concatenates all the arguments of your program.
- * @ac: number of arguments
- * @av: double pointer to arguments
- * Return:pointer to new string, or NULL if error
+ * argstostr - Entry point
+ * @ac: int
+ * @av: Character
+ * Description: concatenates all the arguments of your program
+ * Return: char
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j,  k, n;
-	char *s;
+	int i, j, len = 0, index = 0;
+	char *str;
 
-	k = 0, n = 0;
-	if (ac <= 0 || av == NULL)
+	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
+	}
+	/* Calculate the total length of the concatenated string */
 	for (i = 0; i < ac; i++)
-{
-	for (j = 0; av[i][j]; j++)
-		n++;
-	n++;
-}
-n++;
-s = malloc(n *sizeof(char));
-if (s == NULL)
-	return (NULL);
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+		len++; /* add space for newline */
+	}
+	len++; /* add space for null terminator */
+	str = malloc(len * sizeof(char));
+	/* Allocate memory for the concatenated string */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/* Concatenate the strings */
 	for (i = 0; i < ac; i++)
-{
-	for (j = 0; av[i][j]; j++)
-{
-	s[k] = av[i][j];
-	k++;
-}
-s[k] = '\n';
-k++;
-}
-s[k] = '\0';
-return (s);
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[index++] = av[i][j];
+		}
+		str[index++] = '\n';
+	}
+	str[index] = '\0';
+	return (str);
 }
