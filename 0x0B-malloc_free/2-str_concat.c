3a4646db0a36b31428e3fad54d934d709abc5154
Olabisitee <oladepoolabisi2001@gmail.com>
2023-03-14 18:24:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,63 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ *_strlen - count arrray
+ *@s: array of elements
+ *Return: i
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != ' \0 ')
+		/ Count character of string /
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string
- * @s2: second string
- *
- * Return: a pointer to a newly allocated space in memory which
- * contains the contents of s1, followed by the contents of s2,
- * and null terminated. NULL on failure
+ *str_concat - back a pointer to array
+ *@s1: Array one
+ *@s2: Array two
+ *Return: Always an array dinamic
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2, len;
-	char *result;
+	char *dst;
+	unsigned int i, j, size;
 
-	len1 = len2 = 0;
+	/ If the array is empty /
+	if (s1 == NULL)
+		s1 = "";
 
-	if (s1 != NULL)
-{
-	i = 0;
-	while (s1[i++] != '\0')
-		len1++;
-}
+	if (s2 == NULL)
+		s2 = "";
 
-if (s2 != NULL)
-{
-	i = 0;
-	while (s2[i++] != '\0')
-		len2++;
-}
+	/ count size total /
+		size = (_strlen(s1) + _strlen(s2) + 1);
 
-len = len1 + len2;
-result = (char *)malloc(sizeof(char) * (len + 1));
-if (result == NULL)
-	return (NULL);
+	/ malloc /
+		dst = (char *) malloc(size * sizeof(char));
 
-	for (i = 0; i < len1; i++)
-	result[i] = s1[i];
-	for (j = 0; j < len2; j++, i++)
-	result[i] = s2[j];
-	result[len] = '\0';
+	if (dst == 0)
+	{
+		return (NULL);
+	}
 
-	return (result);
+	/ Concatenate arrays /
+		for (i = 0; *(s1 + i) != '\0'; i++)
+			*(dst + i) = *(s1 + i);
+
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + i) = *(s2 + j);
+		i++;
+	}
+
+	return (dst);
 }
